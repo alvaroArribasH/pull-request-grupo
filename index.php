7d18dc76f2a0363f96461627eb619271032de45c
alvaroArribasH <alvaro.arrher.1@educa.jcyl.es>
2022-11-11 11:27:09 +0100

    rollback

--- index.php
+++ index.php
@@ -6,21 +6,6 @@ echo"
 	<body>
 		<h2>GitHub y pull request</h2>
 	</body>
-	<script>
-    function formatearNombre(){
-        var inputFormulario = document.getElementById("nombre");
-        var nombre = inputFormulario.value;
-        var resultado = nombre.toLowerCase();
-        inputFormulario.value = resultado;
-        return;
-    }
-    function esMayor(){
-        var edad = document.getElementById("edad").value;
-        if(edad < 18){
-            return false;
-        }
-        return true;
-    }
-	</script>
+
 ";
 ?>
