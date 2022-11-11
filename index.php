dfd05765b4ce5069776392d53526135f47a96625
Gonzalo <despliegue@daw2.com>
2022-11-11 10:28:22 +0100

    ultimosCambios1

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-
+//creacion de un formulario
+echo("<form name=Formulario1 action=index.php method=POST>");
+    echo("Nombre<input type=text>");
+    echo("<br>");
+    echo("edad<input type=number>")
+echo("</form>");
 ?>
